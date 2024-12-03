457fe0355494b08dce808f46ea6c89eccdff3407
rainbirduk <104363743+rainbirduk@users.noreply.github.com>
2024-12-03 22:42:02 +0000

    Update main.py
    removed messages from break in main script.

--- python/main.py
+++ python/main.py
@@ -11,11 +11,6 @@ def main():
             menu.run_script(menu.items[user_selection][1])
         
         else:
-            print("")
-            print("Thanks for playing the Monty Hall game")
-            print("This little project is dedicated to Marilyn vos Savant, a magazine columnist", 
-                  "with an IQ of 228 who faced down 1,000 PhD's to solve the Monty Hall problem.")
-            print("")
             break
 
 if __name__ == "__main__":
