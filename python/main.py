05406cbb7496b7bded198a16a31261bf30832394
rainbirduk <104363743+rainbirduk@users.noreply.github.com>
2025-06-20 09:53:42 +0100

    Update main.py
    I changed this line to break the loop only if the user selects 6 (Quit). Any other selection calls the menu function and error handling for invalid entries is handled within that function.

--- python/main.py
+++ python/main.py
@@ -6,7 +6,7 @@ print("WELCOME TO THE MONTY HALL GAME")
 def main():
     while True:
         user_selection = menu.menu()
-        if user_selection < len(menu.items):
+        if user_selection != 6:
             menu.run_script(menu.items[user_selection][1])      
         else:
             print("")
