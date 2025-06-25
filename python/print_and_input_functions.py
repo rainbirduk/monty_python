ea0125f521026bc116d433e278d04651929e3e3c
rainbirduk <104363743+rainbirduk@users.noreply.github.com>
2025-06-25 16:07:09 +0100

    moving pdf report generation to separate function

--- python/print_and_input_functions.py
+++ python/print_and_input_functions.py
@@ -19,7 +19,6 @@ def print_intro():
 
 # print the rules
 def print_rules():
-    print("")
     try:
         with open('../text/game_rules.txt', 'r') as file:
             print(file.read())
@@ -39,6 +38,7 @@ def input_initial_choice():
     print("")
     initial_choice = input("Choose a box by entering a number between 1 and 3, or type 'rules' for a reminder of the rules and assumptions: ")
     while initial_choice == "rules":
+        print("")
         print_rules()
         print("")
         initial_choice = input("Now choose a box by entering a number between 1 and 3: ")
@@ -101,7 +101,6 @@ def print_game_outcome(final_choice, winning_box):
     else:
         print(f"You have lost :( The winning box was box number {box_text_dict[winning_box]}.")
         print("Thanks for playing. Better luck next time.")
-    print("")
 
 # choose the number of simulations to perform
 def choose_n_simulations():
@@ -110,10 +109,10 @@ def choose_n_simulations():
     return n_simulations
 
 # choose the number of rounds to play per simulation
-def choose_n_rounds():
+def choose_n_games():
     print("")
-    n_rounds = int(input("And how many rounds would you like to play per simulation? "))
-    return n_rounds
+    n_games = int(input("And how many games would you like to play per simulation? "))
+    return n_games
 
 # choose whether tactics should be chosen equally or at random. 
 def allocate_tactic():
